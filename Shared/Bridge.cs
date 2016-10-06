fd1dd57db9f5f4ac3e210b61e2c574591a7cc19b
Mikalai Barysenka <mikalai_barysenka@epam.com>
2016-10-06 14:29:57 +0300

    Added sources

---
+++ Shared/Bridge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ReportPortal.Client;
+using ReportPortal.Client.Models;
+using ReportPortal.Client.Requests;
+
+namespace ReportPortal.Shared
+{
+    public static class Bridge
+    {
+        public static Service Service { get; set; }
+
+        private static ContextInfo _context;
+        public static ContextInfo Context
+        {
+            get { return _context ?? (_context = new ContextInfo()); }
+        }
+
+        public static void LogMessage(LogLevel level, string message)
+        {
+            if (Service != null && Context.LaunchId != null)
+            {
+                var request = new AddLogItemRequest
+                {
+                    TestItemId = Context.TestId,
+                    Level = level,
+                    Time = DateTime.UtcNow,
+                    Text = message
+                };
+
+                Service.AddLogItem(request);
+            }
+        }
+    }
+}
---
+++ Shared/ContextInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace ReportPortal.Shared
+{
+    public class ContextInfo
+    {
+        /// <summary>
+        /// Current launch ID.
+        /// </summary>
+        public string LaunchId { get; set; }
+
+        /// <summary>
+        /// Current test item ID.
+        /// </summary>
+        public string TestId { get; set; }
+    }
+}
