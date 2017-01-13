1545365509d8687361eaf179e1d4a39dfa87fd96
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2017-01-13 17:05:28 +0300

    Added simple extension mechanism

--- src/ReportPortal.Shared/Bridge.cs
+++ src/ReportPortal.Shared/Bridge.cs
@@ -2,11 +2,39 @@
 using ReportPortal.Client;
 using ReportPortal.Client.Models;
 using ReportPortal.Client.Requests;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ReportPortal.Shared
 {
     public static class Bridge
     {
+        static Bridge()
+        {
+            var currentDirectory = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            foreach (var file in currentDirectory.GetFiles("ReportPortal.Shared.*Extension.dll"))
+            {
+                AppDomain.CurrentDomain.Load(Path.GetFileNameWithoutExtension(file.Name));
+            }
+
+            foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.GetInterfaces().Contains(typeof(IBridgeExtension)))
+                    {
+                        var extension = Activator.CreateInstance(type);
+                        Extensions.Add((IBridgeExtension)extension);
+                    }
+                }
+            }
+        }
+
+        private static List<IBridgeExtension> Extensions = new List<IBridgeExtension>();
+
         public static Service Service { get; set; }
 
         private static ContextInfo _context;
@@ -17,7 +45,15 @@ namespace ReportPortal.Shared
 
         public static void LogMessage(LogLevel level, string message)
         {
-            if (Service != null && Context.LaunchId != null)
+            if (Extensions.Count != 0)
+            {
+                foreach(var extension in Extensions)
+                {
+                    extension.Log(level, message);
+                }
+            }
+
+            if (Service != null && Context.LaunchId != null && Context.TestId != null)
             {
                 var request = new AddLogItemRequest
                 {
---
+++ src/ReportPortal.Shared/IBridgeExtension.cs
@@ -0,0 +1,14 @@
+﻿using ReportPortal.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportPortal.Shared
+{
+    public interface IBridgeExtension
+    {
+        void Log(LogLevel level, string message);
+    }
+}
