b08078c3a3a14287a2d53a78a6d10626e1113b08
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2017-08-31 15:14:53 +0300

    Update log items to use httpclient

--- src/ReportPortal.Client/Service.LogItem.cs
+++ src/ReportPortal.Client/Service.LogItem.cs
@@ -1,108 +1,109 @@
-﻿//using System.Collections.Generic;
-//using System.Text;
-//using ReportPortal.Client.Extentions;
-//using ReportPortal.Client.Filtering;
-//using ReportPortal.Client.Models;
-//using ReportPortal.Client.Requests;
-//using RestSharp;
-//using System.Threading.Tasks;
-//using ReportPortal.Client.Converters;
+﻿using System.Collections.Generic;
+using System.Text;
+using ReportPortal.Client.Filtering;
+using ReportPortal.Client.Models;
+using ReportPortal.Client.Requests;
+using System.Threading.Tasks;
+using ReportPortal.Client.Converters;
+using System;
+using System.Net.Http;
 
-//namespace ReportPortal.Client
-//{
-//    public partial class Service
-//    {
-//        /// <summary>
-//        /// Returns a list of log items for specified test item.
-//        /// </summary>
-//        /// <param name="filterOption">Specified criterias for retrieving log items.</param>
-//        /// <returns>A list of log items.</returns>
-//        public LogItemsContainer GetLogItems(FilterOption filterOption = null)
-//        {
-//            var request = new RestRequest(Project + "/log");
-//            if (filterOption != null)
-//            {
-//                foreach (var p in filterOption.ConvertToDictionary())
-//                {
-//                    request.AddParameter(p.Key, p.Value);
-//                }
-//            }
-//            var response = _restClient.ExecuteWithErrorHandling(request);
-//            return ModelSerializer.Deserialize<LogItemsContainer>(response.Content);
-//        }
+namespace ReportPortal.Client
+{
+    public partial class Service
+    {
+        /// <summary>
+        /// Returns a list of log items for specified test item.
+        /// </summary>
+        /// <param name="filterOption">Specified criterias for retrieving log items.</param>
+        /// <returns>A list of log items.</returns>
+        public async Task<LogItemsContainer> GetLogItemsAsync(FilterOption filterOption = null)
+        {
+            UriBuilder uriBuilder = new UriBuilder($"{BaseUri}/{Project}/log");
+            
+            if (filterOption != null)
+            {
+                uriBuilder.Query += filterOption;
+            }
 
-//        /// <summary>
-//        /// Returns specified log item by ID.
-//        /// </summary>
-//        /// <param name="id">ID of the log item to retrieve.</param>
-//        /// <returns>A representation of log item/</returns>
-//        public LogItem GetLogItem(string id)
-//        {
-//            var request = new RestRequest(Project + "/log/" + id);
-//            var response = _restClient.ExecuteWithErrorHandling(request);
-//            return ModelSerializer.Deserialize<LogItem>(response.Content);
-//        }
+            var response = await _httpClient.GetAsync(uriBuilder.Uri);
+            response.EnsureSuccessStatusCode();
+            return ModelSerializer.Deserialize<LogItemsContainer>(await response.Content.ReadAsStringAsync());
+        }
 
-//        /// <summary>
-//        /// Returns binary data of attached file to log message.
-//        /// </summary>
-//        /// <param name="id">ID of data.</param>
-//        /// <returns>Array of bytes.</returns>
-//        public byte[] GetBinaryData(string id)
-//        {
-//            var request = new RestRequest(Project + "/data/" + id);
-//            var response = _restClient.Execute(request);
-//            return response.RawBytes;
-//        }
+        /// <summary>
+        /// Returns specified log item by ID.
+        /// </summary>
+        /// <param name="id">ID of the log item to retrieve.</param>
+        /// <returns>A representation of log item/</returns>
+        public async Task<LogItem> GetLogItemAsync(string id)
+        {
+            var uri = $"{Project}/log/{id}";
+            var response = await _httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            return ModelSerializer.Deserialize<LogItem>(await response.Content.ReadAsStringAsync());
+        }
 
-//        /// <summary>
-//        /// Creates a new log item.
-//        /// </summary>
-//        /// <param name="model">Information about representation of log item.</param>
-//        /// <returns>Representation of just created log item.</returns>
-//        public LogItem AddLogItem(AddLogItemRequest model)
-//        {
-//            var request = new RestRequest(Project + "/log/", Method.POST);
-           
-//            if (model.Attach == null)
-//            {
-//                var body = ModelSerializer.Serialize<AddLogItemRequest>(model);
-//                request.AddParameter("application/json", body, ParameterType.RequestBody);
-//            }
-//            else
-//            {
-//                var body = ModelSerializer.Serialize<List<AddLogItemRequest>>(new List<AddLogItemRequest> { model });
-//                request.AddFile("json_request_part", Encoding.Unicode.GetBytes(body), "body", "application/json");
-//                request.AddFile(model.Attach.Name, model.Attach.Data, model.Attach.Name, model.Attach.MimeType);
-//            }
+        /// <summary>
+        /// Returns binary data of attached file to log message.
+        /// </summary>
+        /// <param name="id">ID of data.</param>
+        /// <returns>Array of bytes.</returns>
+        public async Task<byte[]> GetBinaryDataAsync(string id)
+        {
+            var uri = $"{Project}/data/{id}";
+            var response = await _httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsByteArrayAsync();
+        }
 
-//            var response = _restClient.ExecuteWithErrorHandling(request);
-//            var result = model.Attach == null ? ModelSerializer.Deserialize<LogItem>(response.Content) : ModelSerializer.Deserialize<Responses>(response.Content).LogItems[0];
-//            return result;
-//        }
+        /// <summary>
+        /// Creates a new log item.
+        /// </summary>
+        /// <param name="model">Information about representation of log item.</param>
+        /// <returns>Representation of just created log item.</returns>
+        public async Task<LogItem> AddLogItemAsync(AddLogItemRequest model)
+        {
+            var uri = $"{Project}/log";
 
-//        [System.Runtime.Serialization.DataContract]
-//        public class Responses
-//        {
-//            [System.Runtime.Serialization.DataMember(Name = "responses")]
-//            public List<LogItem> LogItems { get; set; }
-//        }
+            if (model.Attach == null)
+            {
+                var body = ModelSerializer.Serialize<AddLogItemRequest>(model);
+                var response = await _httpClient.PostAsync(uri, new StringContent(body, Encoding.UTF8, "application/json"));
+                response.EnsureSuccessStatusCode();
+                return ModelSerializer.Deserialize<LogItem>(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                var body = ModelSerializer.Serialize<List<AddLogItemRequest>>(new List<AddLogItemRequest> { model });
+                var multipartContent = new MultipartFormDataContent();
+                multipartContent.Add(new StringContent(body, Encoding.UTF8, "application/json"), "json_request_part");
+                multipartContent.Add(new ByteArrayContent(model.Attach.Data, 0, model.Attach.Data.Length), "file", model.Attach.Name);
+                var response = await _httpClient.PostAsync(uri, multipartContent);
+                response.EnsureSuccessStatusCode();
+                var c = await response.Content.ReadAsStringAsync();
+                return ModelSerializer.Deserialize<Responses>(c).LogItems[0];
+            }
+        }
 
-//        public async Task<LogItem> AddLogItemAsync(AddLogItemRequest model)
-//        {
-//            return await Task.Run(() => AddLogItem(model));
-//        }
+        [System.Runtime.Serialization.DataContract]
+        public class Responses
+        {
+            [System.Runtime.Serialization.DataMember(Name = "responses")]
+            public List<LogItem> LogItems { get; set; }
+        }
 
-//        /// <summary>
-//        /// Deletes specified log item.
-//        /// </summary>
-//        /// <param name="id">ID of the log item to delete.</param>
-//        /// <returns>A message from service.</returns>
-//        public Message DeleteLogItem(string id)
-//        {
-//            var request = new RestRequest(Project + "/log/" + id, Method.DELETE);
-//            var response = _restClient.ExecuteWithErrorHandling(request);
-//            return ModelSerializer.Deserialize<Message>(response.Content);
-//        }
-//    }
-//}
+        /// <summary>
+        /// Deletes specified log item.
+        /// </summary>
+        /// <param name="id">ID of the log item to delete.</param>
+        /// <returns>A message from service.</returns>
+        public async Task<Message> DeleteLogItemAsync(string id)
+        {
+            var uri = $"{Project}/log/{id}";
+            var response = await _httpClient.DeleteAsync(uri);
+            response.EnsureSuccessStatusCode();
+            return ModelSerializer.Deserialize<Message>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
