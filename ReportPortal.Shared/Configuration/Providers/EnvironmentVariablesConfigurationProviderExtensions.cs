d6d1f7d0e6f314f717b8ebf4061470b4b164b8d1
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-01-09 13:56:29 +0300

    Add env machine scope for env variables

--- ReportPortal.Shared/Configuration/Providers/EnvironmentVariablesConfigurationProviderExtensions.cs
+++ ReportPortal.Shared/Configuration/Providers/EnvironmentVariablesConfigurationProviderExtensions.cs
@@ -7,6 +7,8 @@
 
         public static IConfigurationBuilder AddEnvironmentVariables(this IConfigurationBuilder builder)
         {
+            builder.Add(new EnvironmentVariablesConfigurationProvider(PREFIX, DELIMETER, System.EnvironmentVariableTarget.Machine));
+
             builder.Add(new EnvironmentVariablesConfigurationProvider(PREFIX, DELIMETER, System.EnvironmentVariableTarget.User));
 
             builder.Add(new EnvironmentVariablesConfigurationProvider(PREFIX, DELIMETER, System.EnvironmentVariableTarget.Process));
