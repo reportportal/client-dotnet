2cea1e58501326354700e9eedf61a55ea50b41f4
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-02-08 16:26:53 +0300

    Added MethodNotAllowed to retries list

---
+++ ReportPortal.Client/RetryWithExponentialBackoffHttpClientHandler.cs
@@ -0,0 +1,70 @@
+﻿using ReportPortal.Client.Extentions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReportPortal.Client
+{
+    public class RetryWithExponentialBackoffHttpClientHandler : DelegatingHandler
+    {
+        public int MaxRetries { get; private set; }
+
+        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries)
+            : this(maxRetries, new HttpClientHandler())
+        {
+        }
+
+        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries, IWebProxy proxy)
+            : this(maxRetries, new HttpClientHandler { Proxy = proxy })
+        {
+        }
+
+        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries, HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+            MaxRetries = maxRetries;
+        }
+
+        private List<HttpStatusCode> ResponseStatusCodesForRetrying => new List<HttpStatusCode> { HttpStatusCode.InternalServerError, HttpStatusCode.NotImplemented, HttpStatusCode.BadGateway, HttpStatusCode.ServiceUnavailable, HttpStatusCode.GatewayTimeout, HttpStatusCode.MethodNotAllowed };
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = null;
+
+            for (int i = 0; i < MaxRetries; i++)
+            {
+                try
+                {
+                    response = await base.SendAsync(request, cancellationToken);
+
+                    if (!ResponseStatusCodesForRetrying.Contains(response.StatusCode))
+                    {
+                        return response;
+                    }
+                    else
+                    {
+                        response.VerifySuccessStatusCode();
+                    }
+                }
+
+                catch (Exception exp) when (exp is TaskCanceledException || exp is HttpRequestException)
+                {
+                    if (i < MaxRetries - 1)
+                    {
+                        await Task.Delay((int)Math.Pow(2, i + MaxRetries) * 1000);
+                    }
+
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return response;
+        }
+    }
+}
--- ReportPortal.Client/Service.cs
+++ ReportPortal.Client/Service.cs
@@ -2,9 +2,6 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Collections.Generic;
 using ReportPortal.Client.Extentions;
 
 namespace ReportPortal.Client
@@ -74,65 +71,5 @@ namespace ReportPortal.Client
         public string Project { get; set; }
 
         public Uri BaseUri { get; set; }
-    }
-
-    public class RetryWithExponentialBackoffHttpClientHandler : DelegatingHandler
-    {
-        public int MaxRetries { get; private set; }
-
-        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries)
-            : this(maxRetries, new HttpClientHandler())
-        {
-        }
-
-        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries, IWebProxy proxy)
-            : this(maxRetries, new HttpClientHandler { Proxy = proxy })
-        {
-        }
-
-        public RetryWithExponentialBackoffHttpClientHandler(int maxRetries, HttpMessageHandler innerHandler)
-            : base(innerHandler)
-        {
-            MaxRetries = maxRetries;
-        }
-
-        private List<HttpStatusCode> ResponseStatusCodesForRetrying => new List<HttpStatusCode> { HttpStatusCode.InternalServerError, HttpStatusCode.NotImplemented, HttpStatusCode.BadGateway, HttpStatusCode.ServiceUnavailable, HttpStatusCode.GatewayTimeout };
-
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            HttpResponseMessage response = null;
-
-            for (int i = 0; i < MaxRetries; i++)
-            {
-                try
-                {
-                    response = await base.SendAsync(request, cancellationToken);
-
-                    if (!ResponseStatusCodesForRetrying.Contains(response.StatusCode))
-                    {
-                        return response;
-                    }
-                    else
-                    {
-                        response.VerifySuccessStatusCode();
-                    }
-                }
-
-                catch (Exception exp) when (exp is TaskCanceledException || exp is HttpRequestException)
-                {
-                    if (i < MaxRetries - 1)
-                    {
-                        await Task.Delay((int)Math.Pow(2, i + MaxRetries) * 1000);
-                    }
-
-                    else
-                    {
-                        throw;
-                    }
-                }
-            }
-
-            return response;
-        }
     }
 }
