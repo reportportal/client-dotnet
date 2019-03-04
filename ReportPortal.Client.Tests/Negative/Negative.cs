835c5a0b067ebddc37a62715371b02dd427eb213
vitali-sonchyk-epam <vitali_sonchyk@epam.com>
2019-03-04 19:00:31 +0300

    Fix of Negative feature

--- ReportPortal.Client.Tests/Negative/Negative.cs
+++ ReportPortal.Client.Tests/Negative/Negative.cs
@@ -13,21 +13,21 @@ namespace ReportPortal.Client.Tests.Negative
         public async Task IncorrectHost()
         {
             var service = new Service(new Uri("https://abc.abc/"), "p", "p");
-            await Assert.ThrowsAsync<HttpRequestException>(async () => await Service.LaunchClient.GetLaunchAsync("123"));
+            await Assert.ThrowsAsync<HttpRequestException>(async () => await service.LaunchClient.GetLaunchAsync("123"));
         }
 
         [Fact]
         public async Task IncorrectUrlInCorrectHost()
         {
             var service = new Service(new Uri("https://rp.epam.com/api/blabla/"), "p", "p");
-            await Assert.ThrowsAsync<SerializationException>(async () => await Service.LaunchClient.StartLaunchAsync(new StartLaunchRequest { Name = "abc" }));
+            await Assert.ThrowsAsync<SerializationException>(async () => await service.LaunchClient.StartLaunchAsync(new StartLaunchRequest { Name = "abc" }));
         }
 
         [Fact]
         public async Task IncorrectUuid()
         {
             var service = new Service(new Uri("https://rp.epam.com/api/v1/"), "default_project", "incorrect_uuid");
-            await Assert.ThrowsAsync<HttpRequestException>(async () => await Service.LaunchClient.GetLaunchesAsync());
+            await Assert.ThrowsAsync<HttpRequestException>(async () => await service.LaunchClient.GetLaunchesAsync());
         }
     }
 }
