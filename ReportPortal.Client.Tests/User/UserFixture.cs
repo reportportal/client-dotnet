806a228056bf9268d79a81c2f600ab9ef0864ae1
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-05-13 13:36:09 +0300

    Added assigment projects for user model

--- ReportPortal.Client.Tests/User/UserFixture.cs
+++ ReportPortal.Client.Tests/User/UserFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using ReportPortal.Client.Models;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ReportPortal.Client.Tests.User
@@ -11,6 +12,12 @@ namespace ReportPortal.Client.Tests.User
             var user = await Service.GetUserAsync();
             Assert.Equal("Used for Net integration check via CI", user.Fullname);
             Assert.NotEmpty(user.Email);
+
+            Assert.NotNull(user.AssignedProjects);
+            Assert.NotEmpty(user.AssignedProjects.Keys);
+
+            Assert.Contains("ci-agents-checks", user.AssignedProjects.Keys);
+            Assert.Equal(ProjectRole.Member, user.AssignedProjects["ci-agents-checks"].ProjectRole);
         }
     }
 }
--- ReportPortal.Client/Models/User.cs
+++ ReportPortal.Client/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using ReportPortal.Client.Converters;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace ReportPortal.Client.Models
 {
@@ -10,5 +12,33 @@ namespace ReportPortal.Client.Models
 
         [DataMember(Name = "email")]
         public string Email { get; set; }
+
+        [DataMember(Name = "assigned_projects")]
+        public IDictionary<string, ProjectAssigment> AssignedProjects { get; set; }
+    }
+
+    [DataContract]
+    public class ProjectAssigment
+    {
+        [DataMember(Name = "projectRole")]
+        public string ProjectRoleString { get; set; }
+
+        public ProjectRole ProjectRole
+        {
+            get => EnumConverter.ConvertTo<ProjectRole>(ProjectRoleString);
+            set => ProjectRoleString = EnumConverter.ConvertFrom(value);
+        }
+    }
+
+    public enum ProjectRole
+    {
+        [DataMember(Name = "PROJECT_MANAGER")]
+        ProjectManager,
+        [DataMember(Name = "MEMBER")]
+        Member,
+        [DataMember(Name = "OPERATOR")]
+        Operator,
+        [DataMember(Name = "CUSTOMER")]
+        Customer
     }
 }
