7fc320db2f5691159d855b47572c2d1abfb42012
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-07-04 19:04:33 +0300

    Update token

--- ReportPortal.Client.Tests/BaseFixture.cs
+++ ReportPortal.Client.Tests/BaseFixture.cs
@@ -5,6 +5,6 @@ namespace ReportPortal.Client.Tests
     public class BaseFixture
     {
         protected static readonly string Username = "ci_check_net_client";
-        protected readonly Service Service = new Service(new Uri("https://rp.epam.com/api/v1"), "ci-agents-checks", "b79e81a5-8448-49b5-857d-945ff5fd5ed2");
+        protected readonly Service Service = new Service(new Uri("https://rp.epam.com/api/v1"), "ci-agents-checks", "ba7eb7c8-7b33-42f6-8cf0-e9cd26e717f4");
     }
 }
