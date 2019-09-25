a7c9635fd2cf8600c5c7f82ecb21e710dcbe7b0f
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-09-25 18:21:13 +0300

    Added tests for internal logger

--- test/ReportPortal.Shared.Tests/Internal/LoggerTest.cs
+++ test/ReportPortal.Shared.Tests/Internal/LoggerTest.cs
@@ -1,6 +1,8 @@
 ﻿using ReportPortal.Shared.Internal.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,6 +12,8 @@ namespace ReportPortal.Shared.Tests.Internal
     public class LoggerTest : IDisposable
     {
         private ITestOutputHelper _out;
+
+        private string _defaultLogFilePath = $"ReportPortal.Shared.Tests.{Process.GetCurrentProcess().Id}.log";
 
         public LoggerTest(ITestOutputHelper output)
         {
@@ -33,6 +37,26 @@ namespace ReportPortal.Shared.Tests.Internal
             }
 
             Task.WaitAll(tasks.ToArray());
+
+            Assert.True(File.Exists(_defaultLogFilePath));
+        }
+
+        [Fact]
+        public void ShouldNotAffectDefaultTraceListeners()
+        {
+            Assert.Single(Trace.Listeners);
+        }
+
+        [Fact]
+        public void ShouldNotCaptureDefaultTrace()
+        {
+            TraceLogManager.GetLogger<LoggerTest>().Info("should_see_it");
+
+            Assert.Contains("should_see_it", File.ReadAllText(_defaultLogFilePath));
+
+            Trace.TraceInformation("should_not_see_it");
+
+            Assert.DoesNotContain("should_not_see_it", File.ReadAllText(_defaultLogFilePath));
         }
 
         public void Dispose()
