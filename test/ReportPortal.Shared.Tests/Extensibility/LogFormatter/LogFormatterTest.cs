158e8b20efac8a75e5efe078e99d34f6dfe6ca4d
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-09-26 13:43:18 +0300

    Added tests for LogFomatter and LogHandler

---
+++ test/ReportPortal.Shared.Tests/Extensibility/LogFormatter/LogFormatterTest.cs
@@ -0,0 +1,33 @@
+﻿using ReportPortal.Client.Requests;
+using ReportPortal.Shared.Extensibility;
+using ReportPortal.Shared.Tests.Helpers;
+using Xunit;
+
+namespace ReportPortal.Shared.Tests.Extensibility.LogFormatter
+{
+    public class LogFormatterTest : ILogFormatter
+    {
+        public int Order => 10;
+
+        [Fact]
+        public void ShouldInvokeFormatLogMethod()
+        {
+            var service = new MockServiceBuilder().Build();
+
+            var launchScheduler = new LaunchScheduler(service.Object);
+            var launchReporter = launchScheduler.Build(1, 1, 1);
+
+            launchReporter.Sync();
+
+            Assert.True(Invoked);
+        }
+
+        public bool FormatLog(ref AddLogItemRequest logRequest)
+        {
+            Invoked = true;
+            return false;
+        }
+
+        private static bool Invoked { get; set; }
+    }
+}
---
+++ test/ReportPortal.Shared.Tests/Extensibility/LogHandler/LogHandlerTest.cs
@@ -0,0 +1,36 @@
+﻿using ReportPortal.Client.Requests;
+using ReportPortal.Shared.Extensibility;
+using ReportPortal.Shared.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ReportPortal.Shared.Tests.Extensibility.LogHandler
+{
+    public class LogHandlerTest : ILogHandler
+    {
+        public int Order => 10;
+
+        [Fact]
+        public void ShouldInvokeHandleLogMethod()
+        {
+            var service = new MockServiceBuilder().Build();
+
+            var launchScheduler = new LaunchScheduler(service.Object);
+            var launchReporter = launchScheduler.Build(1, 1, 1);
+
+            launchReporter.Sync();
+
+            Assert.True(Invoked);
+        }
+
+        public bool Handle(AddLogItemRequest logRequest)
+        {
+            Invoked = true;
+            return false;
+        }
+
+        private static bool Invoked { get; set; }
+    }
+}
