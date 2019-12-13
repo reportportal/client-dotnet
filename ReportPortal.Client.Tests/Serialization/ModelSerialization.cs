882c25672171e15615888b3a86ff5cf95d7f64a5
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-12-13 21:36:23 +0300

    New test for serialization with new line character

--- ReportPortal.Client.Tests/Serialization/ModelSerialization.cs
+++ ReportPortal.Client.Tests/Serialization/ModelSerialization.cs
@@ -1,6 +1,7 @@
 ﻿using ReportPortal.Client.Converters;
 using ReportPortal.Client.Models;
 using System;
+using System.Runtime.Serialization;
 using Xunit;
 
 namespace ReportPortal.Client.Tests.Serialization
@@ -14,6 +15,21 @@ namespace ReportPortal.Client.Tests.Serialization
             var exp = Assert.ThrowsAny<Exception>(() => ModelSerializer.Deserialize<Message>(json));
             Assert.Contains(json, exp.Message);
             Assert.NotNull(exp.InnerException);
+        }
+
+        [Fact]
+        public void ShouldDeserializeWithEscapedNewLine()
+        {
+            var json = "{\"P1\": \"abc\\nabc\"}";
+            var a = ModelSerializer.Deserialize<A>(json);
+            Assert.Equal("abc\nabc", a.P1);
+        }
+
+        [DataContract]
+        class A
+        {
+            [DataMember]
+            public string P1 { get; set; }
         }
     }
 }
