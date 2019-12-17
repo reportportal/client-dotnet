b93262773b1f9a0be565b9cbf8eaf51a9091a37e
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-12-17 16:32:40 +0300

    Added Linear func retrier

---
+++ src/ReportPortal.Shared/Internal/Delegating/LinearRetryRequestExecuter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReportPortal.Shared.Internal.Delegating
+{
+    /// <summary>
+    /// Invokes given func with retry strategy and linear delay between attempts.
+    /// </summary>
+    public class LinearRetryRequestExecuter : IRequestExecuter
+    {
+        private Logging.ITraceLogger TraceLogger { get; } = Logging.TraceLogManager.GetLogger<LinearRetryRequestExecuter>();
+
+        private SemaphoreSlim _concurrentAwaiter;
+
+        /// <summary>
+        /// Initializes new instance of <see cref="LinearRetryRequestExecuter"/>.
+        /// </summary>
+        /// <param name="maxConcurrentRequests">Limitation of concurrent func invocation.</param>
+        /// <param name="maxRetryAttempts">Maximum number of attempts.</param>
+        /// <param name="delay">Delay between ateempts (in milliseconds).</param>
+        public LinearRetryRequestExecuter(int maxConcurrentRequests, int maxRetryAttempts, int delay)
+        {
+            if (maxRetryAttempts < 1)
+            {
+                throw new ArgumentException("Maximum attempts cannot be less than 1.", nameof(maxRetryAttempts));
+            }
+
+            if (delay < 0)
+            {
+                throw new ArgumentException("Delay cannot be less than 0.", nameof(delay));
+            }
+
+            _concurrentAwaiter = new SemaphoreSlim(maxConcurrentRequests);
+            MaxRetryAttemps = maxRetryAttempts;
+            Delay = delay;
+        }
+
+        /// <summary>
+        /// Maximum number of attempts
+        /// </summary>
+        public int MaxRetryAttemps { get; private set; }
+
+        /// <summary>
+        /// How many milliseconds to wait between attempts
+        /// </summary>
+        public int Delay { get; private set; }
+
+        /// <inheritdoc/>
+        public async Task<T> ExecuteAsync<T>(Func<Task<T>> func)
+        {
+            T result = default;
+
+            for (int i = 0; i < MaxRetryAttemps; i++)
+            {
+                try
+                {
+                    TraceLogger.Verbose($"Awaiting free executor for {func.Method.Name} method. Available: {_concurrentAwaiter.CurrentCount}");
+                    await _concurrentAwaiter.WaitAsync().ConfigureAwait(false);
+                    TraceLogger.Verbose($"Invoking {func.Method.Name} method... Current attempt: {i}");
+                    result = await func().ConfigureAwait(false);
+                    break;
+                }
+                catch (Exception exp) when (exp is TaskCanceledException || exp is HttpRequestException)
+                {
+                    if (i < MaxRetryAttemps - 1)
+                    {
+                        TraceLogger.Error($"Error while invoking '{func.Method.Name}' method. Current attempt: {i}. Waiting {Delay} milliseconds and retrying it.\n{exp}");
+                        await Task.Delay(Delay).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        TraceLogger.Error($"Error while invoking '{func.Method.Name}' method. Current attempt: {i}.\n{exp}");
+                        throw;
+                    }
+                }
+                catch (Exception exp)
+                {
+                    TraceLogger.Error($"Unexpected exception: {exp}");
+                    throw;
+                }
+                finally
+                {
+                    _concurrentAwaiter.Release();
+                };
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/ReportPortal.Shared.Tests/Internal/Delegating/LinearRetryRequestExecuterTest.cs
@@ -0,0 +1,74 @@
+﻿using FluentAssertions;
+using Moq;
+using ReportPortal.Shared.Internal.Delegating;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReportPortal.Shared.Tests.Internal.Delegating
+{
+    public class LinearRetryRequestExecuterTest
+    {
+        [Fact]
+        public void DelayShouldBeGreaterOrEqualZero()
+        {
+            Action ctor = () => new LinearRetryRequestExecuter(int.MaxValue, 1, delay: -1);
+            ctor.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void MaxAttemptsShouldBeGreaterOrEqualOne()
+        {
+            Action ctor = () => new LinearRetryRequestExecuter(int.MaxValue, maxRetryAttempts: 0, 0);
+            ctor.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public async Task ExecuteValidActionOneTime()
+        {
+            var action = new Mock<Func<Task<string>>>();
+
+            var executer = new LinearRetryRequestExecuter(int.MaxValue, 3, 2);
+            var res = await executer.ExecuteAsync(action.Object);
+            res.Should().Be(null);
+            action.Verify(a => a(), Times.Once);
+        }
+
+        [Fact]
+        public void ShouldRetryTaskCanceledExceptionAction()
+        {
+            var action = new Mock<Func<Task<string>>>();
+            action.Setup(a => a()).Throws<TaskCanceledException>();
+
+            var executer = new LinearRetryRequestExecuter(int.MaxValue, 3, 0);
+            executer.Awaiting(e => e.ExecuteAsync(action.Object)).Should().Throw<TaskCanceledException>();
+
+            action.Verify(a => a(), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void ShouldRetryHttpRequestExceptionAction()
+        {
+            var action = new Mock<Func<Task<string>>>();
+            action.Setup(a => a()).Throws<HttpRequestException>();
+
+            var executer = new LinearRetryRequestExecuter(int.MaxValue, 3, 0);
+            executer.Awaiting(e => e.ExecuteAsync(action.Object)).Should().Throw<HttpRequestException>();
+
+            action.Verify(a => a(), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void ShouldNotRetryAnyOtherExceptionAction()
+        {
+            var action = new Mock<Func<Task<string>>>();
+            action.Setup(a => a()).Throws<Exception>();
+
+            var executer = new LinearRetryRequestExecuter(int.MaxValue, 3, 0);
+            executer.Awaiting(e => e.ExecuteAsync(action.Object)).Should().Throw<Exception>();
+
+            action.Verify(a => a(), Times.Exactly(1));
+        }
+    }
+}
