5e7163bc667254a313157ab2b7691700ff2f6f02
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2019-12-17 18:07:36 +0300

    Configurable retry strategy

--- src/ReportPortal.Shared/Internal/Delegating/RequestExecuterFactory.cs
+++ src/ReportPortal.Shared/Internal/Delegating/RequestExecuterFactory.cs
@@ -16,7 +16,26 @@ namespace ReportPortal.Shared.Internal.Delegating
 
             var maxServiceConnections = configuration.GetValue("Server:MaximumConnectionsNumber", int.MaxValue);
 
-            return new ExponentialRetryRequestExecuter(maxServiceConnections, 3, 2);
+            var defaultStrategyValue = "exponential";
+
+            var retryStrategy = configuration.GetValue("Server:Retry:Strategy", defaultStrategyValue) ?? defaultStrategyValue;
+
+            IRequestExecuter executer;
+            switch (retryStrategy.ToLowerInvariant())
+            {
+                case "exponential":
+                    // TODO: configurable values
+                    executer = new ExponentialRetryRequestExecuter(maxServiceConnections, 3, 2);
+                    break;
+                case "linear":
+                    // TODO: configurable values
+                    executer = new LinearRetryRequestExecuter(maxServiceConnections, 3, 5 * 1000);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Server:Retry:Strategy", $"Unknown '{retryStrategy}' retry strategy. Possible values are 'exponential' and 'linear'.");
+            }
+
+            return executer;
         }
     }
 }
--- test/ReportPortal.Shared.Tests/Internal/Delegating/RequestExecuterFactoryTest.cs
+++ test/ReportPortal.Shared.Tests/Internal/Delegating/RequestExecuterFactoryTest.cs
@@ -28,5 +28,29 @@ namespace ReportPortal.Shared.Tests.Internal.Delegating
             exponentialExecuter.MaxRetryAttemps.Should().Be(3);
             exponentialExecuter.BaseIndex.Should().Be(2);
         }
+
+        [Fact]
+        public void ShouldCreateLinearExecuter()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(c => c.GetValue("Server:Retry:Strategy", It.IsAny<string>())).Returns("linear");
+            var factory = new RequestExecuterFactory();
+            var executer = factory.Create(configuration.Object);
+
+            executer.Should().BeOfType<LinearRetryRequestExecuter>();
+            var exponentialExecuter = executer as LinearRetryRequestExecuter;
+            exponentialExecuter.MaxRetryAttemps.Should().Be(3);
+            exponentialExecuter.Delay.Should().Be(5000);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionForUnknownStrategy()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(c => c.GetValue("Server:Retry:Strategy", It.IsAny<string>())).Returns("any_unknown_value");
+            var factory = new RequestExecuterFactory();
+
+            factory.Invoking((f) => f.Create(configuration.Object)).Should().Throw<ArgumentOutOfRangeException>().WithMessage("*any_unknown_value*");
+        }
     }
 }
