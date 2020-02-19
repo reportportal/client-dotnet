68093439199e5964c7b22fb8523ad3a83081fba9
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-02-19 13:44:58 +0300

    Simplify datetime converter

--- src/ReportPortal.Client/Converters/DateTimeConverter.cs
+++ src/ReportPortal.Client/Converters/DateTimeConverter.cs
@@ -4,15 +4,17 @@ namespace ReportPortal.Client.Converters
 {
     public class DateTimeConverter
     {
+        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
         public static DateTime ConvertTo(string dateString)
         {
             var doubleDate = double.Parse(dateString);
-            return new DateTime(1970, 1, 1).AddMilliseconds(doubleDate);
+            return UNIX_EPOCH.AddMilliseconds(doubleDate);
         }
 
         public static string ConvertFrom(DateTime date)
         {
-            return ((long)(date - new DateTime(1970, 1, 1)).TotalMilliseconds).ToString();
+            return ((long)date.Subtract(UNIX_EPOCH).TotalMilliseconds).ToString();
         }
     }
 }
