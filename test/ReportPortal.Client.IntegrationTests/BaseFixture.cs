12257d5453ff3de996c4082210f043d814860725
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-03-24 12:35:44 +0300

    Update api token

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -8,6 +8,6 @@ namespace ReportPortal.Client.IntegrationTests
         //protected readonly Service Service = new Service(new Uri("https://rp.epam.com/api/v1"), "ci-agents-checks", "ba7eb7c8-7b33-42f6-8cf0-e9cd26e717f4");
 
         protected static readonly string Username = "ci_check_net_client";
-        protected readonly Service Service = new Service(new Uri("http://beta.demo.reportportal.io/api/v1"), "default_personal", "2908ac62-6855-4f70-bd38-64d5589a4073");
+        protected readonly Service Service = new Service(new Uri("http://beta.demo.reportportal.io/api/v1"), "default_personal", "72606fd9-3b79-4ceb-b6e7-df6c5b2a94ae");
     }
 }
