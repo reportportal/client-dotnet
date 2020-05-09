ed6551cc943a5ba3db6e4607ebd062850ff781e6
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-05-09 17:28:24 +0300

    Remove extra http call in test

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -11,6 +11,6 @@ namespace ReportPortal.Client.IntegrationTests
         //protected readonly Service Service = new Service(new Uri("http://beta.demo.reportportal.io/api/v1"), "default_personal", "72606fd9-3b79-4ceb-b6e7-df6c5b2a94ae");
 
         protected static readonly string Username = "ci_check_net_client";
-        protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), "default_personal", "a096fda9-8ba3-4ce9-94c9-f5d2c0b12598");
+        protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), "default_personal", "26f171a9-8bb2-45e8-9e0b-cd75bb7de670");
     }
 }
