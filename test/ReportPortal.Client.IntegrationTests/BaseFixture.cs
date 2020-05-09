90ff50390817a732047eda662b2713675e0707f8
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-05-09 18:02:02 +0300

    Use alpha instance for integration tests

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -11,6 +11,6 @@ namespace ReportPortal.Client.IntegrationTests
         //protected readonly Service Service = new Service(new Uri("http://beta.demo.reportportal.io/api/v1"), "default_personal", "72606fd9-3b79-4ceb-b6e7-df6c5b2a94ae");
 
         protected static readonly string Username = "ci_check_net_client";
-        protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), "default_personal", "26f171a9-8bb2-45e8-9e0b-cd75bb7de670");
+        protected readonly Service Service = new Service(new Uri("https://alpha.reportportal.io/api/v1"), "default_personal", "cd9c39d6-c9a2-45b0-8e48-c4f0151114d0");
     }
 }
