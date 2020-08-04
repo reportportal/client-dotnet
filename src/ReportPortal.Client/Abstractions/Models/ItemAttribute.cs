e3d756c1116c189bda3bd42bebd5d6cbc3027d67
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2020-08-04 22:28:09 +0300

    Trim item attribute key/value

--- src/ReportPortal.Client/Abstractions/Models/ItemAttribute.cs
+++ src/ReportPortal.Client/Abstractions/Models/ItemAttribute.cs
@@ -1,15 +1,23 @@
-﻿using System.Runtime.Serialization;
+﻿using ReportPortal.Client.Converters;
+using System.Runtime.Serialization;
 
 namespace ReportPortal.Client.Abstractions.Models
 {
     [DataContract]
     public class ItemAttribute
     {
+        public const int MAX_KEY_SIZE = 128;
+        public const int MAX_VALUE_SIZE = 128;
+
+        private string _key;
+
         [DataMember(Name = "key")]
-        public string Key { get; set; }
+        public string Key { get { return _key; } set { _key = StringTrimmer.Trim(value, MAX_KEY_SIZE); } }
+
+        private string _value;
 
         [DataMember(Name = "value")]
-        public string Value { get; set; }
+        public string Value { get { return _value; } set { _value = StringTrimmer.Trim(value, MAX_VALUE_SIZE); } }
 
         [DataMember(Name = "system")]
         public bool IsSystem { get; set; }
