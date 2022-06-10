70e86dde2038a59729cb84d48166a5bdd219cfdd
Aleh Yanushkevich <aleh_yanushkevich2@epam.com>
2022-06-10 22:57:10 +0300

    FIxed uuid

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -6,7 +6,7 @@ namespace ReportPortal.Client.IntegrationTests
     {
         protected static readonly string Username = "default";
         protected static readonly string ProjectName = "default_personal";
-        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "ea635fc0-4028-417a-8c29-7a83f6c77a49");
+        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "56e4653a-6c59-40a0-8bd7-08d663eafb7c");
 
         //protected static readonly string Username = "default";
         //protected static readonly string ProjectName = "default_personal";
