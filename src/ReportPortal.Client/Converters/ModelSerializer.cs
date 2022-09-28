1e6bf51c78cccf6b1466301ff02e61c3f0c8bbfb
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-09-28 22:46:33 +0300

    Remove ValueTask in other places

--- src/ReportPortal.Client/Converters/ModelSerializer.cs
+++ src/ReportPortal.Client/Converters/ModelSerializer.cs
@@ -7,12 +7,12 @@ namespace ReportPortal.Client.Converters
 {
     internal class ModelSerializer
     {
-        public static async ValueTask<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default)
+        public static async Task<T> DeserializeAsync<T>(Stream stream, CancellationToken cancellationToken = default)
         {
             return (T)await JsonSerializer.DeserializeAsync(stream, typeof(T), ClientSourceGenerationContext.Default, cancellationToken);
         }
 
-        public static async ValueTask SerializeAsync<T>(object obj, Stream stream, CancellationToken cancellationToken = default)
+        public static async Task SerializeAsync<T>(object obj, Stream stream, CancellationToken cancellationToken = default)
         {
             await JsonSerializer.SerializeAsync(stream, obj, typeof(T), ClientSourceGenerationContext.Default, cancellationToken);
         }
