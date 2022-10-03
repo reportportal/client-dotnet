3b17aafac2ff4ba12843ebd256b12868dc909694
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-10-03 15:06:56 +0300

    Update api token for integration tests

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -6,7 +6,7 @@ namespace ReportPortal.Client.IntegrationTests
     {
         protected static readonly string Username = "default";
         protected static readonly string ProjectName = "default_personal";
-        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "4b035f71-4067-4a10-a2e3-60bba9c19ba0");
+        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "aeb09a3b-18ef-4e9e-b647-4645c0e9a74e");
         //protected static readonly string Username = "default";
         //protected static readonly string ProjectName = "default_personal";
         //protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), ProjectName, "d562c898-7705-49a4-be6d-17a8121715fa");
