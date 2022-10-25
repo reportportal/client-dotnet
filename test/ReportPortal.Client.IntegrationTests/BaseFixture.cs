53b822cf412b512726092816c6570fe610f81295
AlehYanushklevich <103627305+Aleh-Yanushkevich@users.noreply.github.com>
2022-10-25 22:39:35 +0300

    Replace AppVeyor on GitHub Actions (#89)

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -6,7 +6,7 @@ namespace ReportPortal.Client.IntegrationTests
     {
         protected static readonly string Username = "default";
         protected static readonly string ProjectName = "default_personal";
-        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "0e80c23b-9676-4b6b-8255-18ef1dece8b5");
+        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "55e73337-9b6a-4bf2-90eb-dc9567df9368");
         //protected static readonly string Username = "default";
         //protected static readonly string ProjectName = "default_personal";
         //protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), ProjectName, "d562c898-7705-49a4-be6d-17a8121715fa");
