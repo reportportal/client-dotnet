ee9ad0af22844fedbc8ff031b4536872d6aba113
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2022-10-25 14:04:40 +0300

    Update BaseFixture.cs

--- test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
+++ test/ReportPortal.Client.IntegrationTests/BaseFixture.cs
@@ -6,7 +6,7 @@ namespace ReportPortal.Client.IntegrationTests
     {
         protected static readonly string Username = "default";
         protected static readonly string ProjectName = "default_personal";
-        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "aeb09a3b-18ef-4e9e-b647-4645c0e9a74e");
+        protected readonly Service Service = new Service(new Uri("https://demo.reportportal.io/api/v1"), ProjectName, "55e73337-9b6a-4bf2-90eb-dc9567df9368");
         //protected static readonly string Username = "default";
         //protected static readonly string ProjectName = "default_personal";
         //protected readonly Service Service = new Service(new Uri("http://localhost:8080/api/v1"), ProjectName, "d562c898-7705-49a4-be6d-17a8121715fa");
