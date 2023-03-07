3c03f1676f6a6a001f949931a6ff8a594cc5a27a
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-03-07 13:55:34 +0300

    Clean user filter resource

--- src/ReportPortal.Client/Abstractions/Resources/IUserFilterResource.cs
+++ src/ReportPortal.Client/Abstractions/Resources/IUserFilterResource.cs
@@ -15,63 +15,48 @@ namespace ReportPortal.Client.Abstractions.Resources
         /// Creates the specified user filter.
         /// </summary>
         /// <param name="request">Request with user filter for create.</param>
-        /// <returns>Response with Id of created user filter.</returns>
-        Task<UserFilterCreatedResponse> CreateAsync(CreateUserFilterRequest request);
-
-        /// <inheritdoc cref="CreateAsync(CreateUserFilterRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<UserFilterCreatedResponse> CreateAsync(CreateUserFilterRequest request, CancellationToken cancellationToken);
+        /// <returns>Response with Id of created user filter.</returns>
+        Task<UserFilterCreatedResponse> CreateAsync(CreateUserFilterRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Updates the specified user filter.
         /// </summary>
         /// <param name="id">ID of filter to update.</param>
         /// <param name="request">Request with user filter for update</param>
-        /// <returns>Response with info about status of updating user filter.</returns>
-        Task<MessageResponse> UpdateAsync(long id, UpdateUserFilterRequest request);
-
-        /// <inheritdoc cref="UpdateAsync(long, UpdateUserFilterRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<MessageResponse> UpdateAsync(long id, UpdateUserFilterRequest request, CancellationToken cancellationToken);
+        /// <returns>Response with info about status of updating user filter.</returns>
+        Task<MessageResponse> UpdateAsync(long id, UpdateUserFilterRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Gets all user filters.
         /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
         /// <returns>Filtered user filters.</returns>
-        Task<Content<UserFilterResponse>> GetAsync();
+        Task<Content<UserFilterResponse>> GetAsync(CancellationToken cancellationToken = default);
 
-        /// <inheritdoc cref="GetAsync()"/>
-        /// <param name="filterOption">Filter criteria for user filters.</param>
-        Task<Content<UserFilterResponse>> GetAsync(FilterOption filterOption);
-
-        /// <inheritdoc cref="GetAsync()"/>
+        /// <summary>
+        /// Gets all user filters.
+        /// </summary>
+        /// /// <param name="filterOption">Filter criteria for user filters.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<UserFilterResponse>> GetAsync(CancellationToken cancellationToken);
-
-        /// <inheritdoc cref="GetAsync(FilterOption)"/>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<UserFilterResponse>> GetAsync(FilterOption filterOption, CancellationToken cancellationToken);
+        /// <returns>Filtered user filters.</returns>
+        Task<Content<UserFilterResponse>> GetAsync(FilterOption filterOption, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Gets user filter by id.
         /// </summary>
         /// <param name="id">Id of user filter.</param>
-        /// <returns>User filter with specified id.</returns>
-        Task<UserFilterResponse> GetAsync(long id);
-
-        /// <inheritdoc cref="GetAsync(long)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<UserFilterResponse> GetAsync(long id, CancellationToken cancellationToken);
+        /// <returns>User filter with specified id.</returns>
+        Task<UserFilterResponse> GetAsync(long id, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Deletes the specified filter by id.
         /// </summary>
         /// <param name="id">Id of user filter</param>
-        /// <returns>Response with info about user filter deletion status.</returns>
-        Task<MessageResponse> DeleteAsync(long id);
-
-        /// <inheritdoc cref="DeleteAsync(long)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<MessageResponse> DeleteAsync(long id, CancellationToken cancellationToken);
+        /// <returns>Response with info about user filter deletion status.</returns>
+        Task<MessageResponse> DeleteAsync(long id, CancellationToken cancellationToken = default);
     }
 }
