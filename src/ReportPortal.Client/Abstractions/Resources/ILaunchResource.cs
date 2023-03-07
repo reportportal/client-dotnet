6051e406250d6d6a8705f0e48fc1135960b8afbd
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-03-07 15:06:17 +0300

    Clean launch resource

--- src/ReportPortal.Client/Abstractions/Resources/ILaunchResource.cs
+++ src/ReportPortal.Client/Abstractions/Resources/ILaunchResource.cs
@@ -14,138 +14,106 @@ namespace ReportPortal.Client.Abstractions.Resources
         /// <summary>
         /// Analyzes launches.
         /// </summary>
-        /// <returns>A message from service.</returns>
-        Task<MessageResponse> AnalyzeAsync(AnalyzeLaunchRequest request);
-
-        /// <inheritdoc cref="AnalyzeAsync(AnalyzeLaunchRequest)"/>
+        /// <param name="request">A request how to analyze the launch.</param>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<MessageResponse> AnalyzeAsync(AnalyzeLaunchRequest request, CancellationToken cancellationToken);
+        /// <returns>A message from service.</returns>
+        Task<MessageResponse> AnalyzeAsync(AnalyzeLaunchRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Finishes specified launch.
         /// </summary>
         /// <param name="uuid">UUID of specified launch.</param>
         /// <param name="request">Information about representation of launch to finish.</param>
-        /// <returns>A message from service.</returns>
-        Task<LaunchFinishedResponse> FinishAsync(string uuid, FinishLaunchRequest request);
-
-        /// <inheritdoc cref="FinishAsync(string, FinishLaunchRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchFinishedResponse> FinishAsync(string uuid, FinishLaunchRequest request, CancellationToken cancellationToken);
+        /// <returns>A message from service.</returns>
+        Task<LaunchFinishedResponse> FinishAsync(string uuid, FinishLaunchRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Deletes specified launch.
         /// </summary>
         /// <param name="id">ID of the launch to delete.</param>
-        /// <returns>A message from service.</returns>
-        Task<MessageResponse> DeleteAsync(long id);
-
-        /// <inheritdoc cref="DeleteAsync(long)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<MessageResponse> DeleteAsync(long id, CancellationToken cancellationToken);
+        /// <returns>A message from service.</returns>
+        Task<MessageResponse> DeleteAsync(long id, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Returns specified launch by ID.
         /// </summary>
         /// <param name="id">ID of the launch to retrieve.</param>
-        /// <returns>A representation of launch.</returns>
-        Task<LaunchResponse> GetAsync(long id);
-
-        /// <inheritdoc cref="GetAsync(long)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchResponse> GetAsync(long id, CancellationToken cancellationToken);
+        /// <returns>A representation of launch.</returns>
+        Task<LaunchResponse> GetAsync(long id, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Returns specified launch by UUID.
         /// </summary>
         /// <param name="uuid">UUID of the launch to retrieve.</param>
-        /// <returns>A representation of launch.</returns>
-        Task<LaunchResponse> GetAsync(string uuid);
-
-        /// <inheritdoc cref="GetAsync(long)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchResponse> GetAsync(string uuid, CancellationToken cancellationToken);
+        /// <returns>A representation of launch.</returns>
+        Task<LaunchResponse> GetAsync(string uuid, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Returns a list of launches for current project.
         /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
         /// <returns>A list of launches.</returns>
-        Task<Content<LaunchResponse>> GetAsync();
+        Task<Content<LaunchResponse>> GetAsync(CancellationToken cancellationToken = default);
 
-        /// <inheritdoc cref="GetAsync()"/>
+        /// <summary>
+        /// Returns a list of launches for current project.
+        /// </summary>
         /// <param name="filterOption">Specified criterias for retrieving launches.</param>
-        Task<Content<LaunchResponse>> GetAsync(FilterOption filterOption);
-
-        /// <inheritdoc cref="GetAsync()"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<LaunchResponse>> GetAsync(CancellationToken cancellationToken);
-
-        /// <inheritdoc cref="GetAsync(FilterOption)"/>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<LaunchResponse>> GetAsync(FilterOption filterOption, CancellationToken cancellationToken);
+        /// <returns>A list of launches.</returns>
+        Task<Content<LaunchResponse>> GetAsync(FilterOption filterOption, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Returns a list of debug launches for current project.
         /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
         /// <returns>A list of launches.</returns>
-        Task<Content<LaunchResponse>> GetDebugAsync();
+        Task<Content<LaunchResponse>> GetDebugAsync(CancellationToken cancellationToken = default);
 
-        /// <inheritdoc cref="GetDebugAsync()"/>
+        /// <summary>
+        /// Returns a list of debug launches for current project.
+        /// </summary>
         /// <param name="filterOption">Specified criterias for retrieving launches.</param>
-        Task<Content<LaunchResponse>> GetDebugAsync(FilterOption filterOption);
-
-        /// <inheritdoc cref="GetDebugAsync()"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<LaunchResponse>> GetDebugAsync(CancellationToken cancellationToken);
-
-        /// <inheritdoc cref="GetDebugAsync(FilterOption)"/>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        Task<Content<LaunchResponse>> GetDebugAsync(FilterOption filterOption, CancellationToken cancellationToken);
+        /// <returns>A list of launches.</returns>
+        Task<Content<LaunchResponse>> GetDebugAsync(FilterOption filterOption, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Merge several launches.
         /// </summary>
         /// <param name="request">Request for merging.</param>
-        /// <returns>Returns the model of merged launches.</returns>
-        Task<LaunchResponse> MergeAsync(MergeLaunchesRequest request);
-
-        /// <inheritdoc cref="MergeAsync(MergeLaunchesRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchResponse> MergeAsync(MergeLaunchesRequest request, CancellationToken cancellationToken);
+        /// <returns>Returns the model of merged launches.</returns>
+        Task<LaunchResponse> MergeAsync(MergeLaunchesRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Starts new launch.
         /// </summary>
         /// <param name="request">Information about launch.</param>
-        /// <returns>Information about started launch.</returns>
-        Task<LaunchCreatedResponse> StartAsync(StartLaunchRequest request);
-
-        /// <inheritdoc cref="StartAsync(StartLaunchRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchCreatedResponse> StartAsync(StartLaunchRequest request, CancellationToken cancellationToken);
+        /// <returns>Information about started launch.</returns>
+        Task<LaunchCreatedResponse> StartAsync(StartLaunchRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Stopes specified launch even if inner tests are not finished yet.
         /// </summary>
         /// <param name="id">ID of specified launch.</param>
         /// <param name="request">Information about representation of launch to finish.</param>
-        /// <returns>A message from service.</returns>
-        Task<LaunchFinishedResponse> StopAsync(long id, FinishLaunchRequest request);
-
-        /// <inheritdoc cref="StopAsync(long, FinishLaunchRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<LaunchFinishedResponse> StopAsync(long id, FinishLaunchRequest request, CancellationToken cancellationToken);
+        /// <returns>A message from service.</returns>
+        Task<LaunchFinishedResponse> StopAsync(long id, FinishLaunchRequest request, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Update specified launch.
         /// </summary>
         /// <param name="id">ID of launch to update.</param>
         /// <param name="request">Information about launch.</param>
-        /// <returns>A message from service.</returns>
-        Task<MessageResponse> UpdateAsync(long id, UpdateLaunchRequest request);
-
-        /// <inheritdoc cref="UpdateAsync(long, UpdateLaunchRequest)"/>
         /// <param name="cancellationToken">Cancellation token.</param>
-        Task<MessageResponse> UpdateAsync(long id, UpdateLaunchRequest request, CancellationToken cancellationToken);
+        /// <returns>A message from service.</returns>
+        Task<MessageResponse> UpdateAsync(long id, UpdateLaunchRequest request, CancellationToken cancellationToken = default);
     }
 }
--- src/ReportPortal.Client/Resources/ServiceLaunchResource.cs
+++ src/ReportPortal.Client/Resources/ServiceLaunchResource.cs
@@ -14,29 +14,14 @@ namespace ReportPortal.Client.Resources
         {
         }
 
-        public Task<LaunchResponse> GetAsync(string uuid)
-        {
-            return GetAsync(uuid, CancellationToken.None);
-        }
-
         public async Task<LaunchResponse> GetAsync(string uuid, CancellationToken cancellationToken)
         {
             return await GetAsJsonAsync<LaunchResponse>($"v1/{ProjectName}/launch/uuid/{uuid}", cancellationToken).ConfigureAwait(false);
         }
 
-        public Task<LaunchResponse> GetAsync(long id)
-        {
-            return GetAsync(id, CancellationToken.None);
-        }
-
         public async Task<LaunchResponse> GetAsync(long id, CancellationToken cancellationToken)
         {
             return await GetAsJsonAsync<LaunchResponse>($"v1/{ProjectName}/launch/{id}", cancellationToken).ConfigureAwait(false);
-        }
-
-        public Task<LaunchCreatedResponse> StartAsync(StartLaunchRequest request)
-        {
-            return StartAsync(request, CancellationToken.None);
         }
 
         public async Task<LaunchCreatedResponse> StartAsync(StartLaunchRequest request, CancellationToken cancellationToken)
@@ -45,20 +30,10 @@ namespace ReportPortal.Client.Resources
                 $"v1/{ProjectName}/launch", request, cancellationToken).ConfigureAwait(false);
         }
 
-        public Task<LaunchFinishedResponse> FinishAsync(string uuid, FinishLaunchRequest request)
-        {
-            return FinishAsync(uuid, request, CancellationToken.None);
-        }
-
         public async Task<LaunchFinishedResponse> FinishAsync(string uuid, FinishLaunchRequest request, CancellationToken cancellationToken)
         {
             return await PutAsJsonAsync<LaunchFinishedResponse, FinishLaunchRequest>(
                 $"v1/{ProjectName}/launch/{uuid}/finish", request, cancellationToken).ConfigureAwait(false);
-        }
-
-        public Task<LaunchFinishedResponse> StopAsync(long id, FinishLaunchRequest request)
-        {
-            return StopAsync(id, request, CancellationToken.None);
         }
 
         public async Task<LaunchFinishedResponse> StopAsync(long id, FinishLaunchRequest request, CancellationToken cancellationToken)
@@ -67,19 +42,9 @@ namespace ReportPortal.Client.Resources
                 $"v1/{ProjectName}/launch/{id}/stop", request, cancellationToken).ConfigureAwait(false);
         }
 
-        public Task<MessageResponse> DeleteAsync(long id)
-        {
-            return DeleteAsync(id, CancellationToken.None);
-        }
-
         public async Task<MessageResponse> DeleteAsync(long id, CancellationToken cancellationToken)
         {
             return await DeleteAsJsonAsync<MessageResponse>($"v1/{ProjectName}/launch/{id}", cancellationToken).ConfigureAwait(false);
-        }
-
-        public Task<LaunchResponse> MergeAsync(MergeLaunchesRequest request)
-        {
-            return MergeAsync(request, CancellationToken.None);
         }
 
         public async Task<LaunchResponse> MergeAsync(MergeLaunchesRequest request, CancellationToken cancellationToken)
@@ -88,36 +53,16 @@ namespace ReportPortal.Client.Resources
                 $"v1/{ProjectName}/launch/merge", request, cancellationToken).ConfigureAwait(false);
         }
 
-        public Task<MessageResponse> UpdateAsync(long id, UpdateLaunchRequest request)
-        {
-            return UpdateAsync(id, request, CancellationToken.None);
-        }
-
         public async Task<MessageResponse> UpdateAsync(long id, UpdateLaunchRequest request, CancellationToken cancellationToken)
         {
             return await PutAsJsonAsync<MessageResponse, UpdateLaunchRequest>(
                 $"v1/{ProjectName}/launch/{id}/update", request, cancellationToken).ConfigureAwait(false);
         }
 
-        public Task<MessageResponse> AnalyzeAsync(AnalyzeLaunchRequest request)
-        {
-            return AnalyzeAsync(request, CancellationToken.None);
-        }
-
         public async Task<MessageResponse> AnalyzeAsync(AnalyzeLaunchRequest request, CancellationToken cancellationToken)
         {
             return await PostAsJsonAsync<MessageResponse, AnalyzeLaunchRequest>(
                 $"v1/{ProjectName}/launch/analyze", request, cancellationToken).ConfigureAwait(false);
-        }
-
-        public Task<Content<LaunchResponse>> GetAsync()
-        {
-            return GetAsync(filterOption: null, CancellationToken.None);
-        }
-        
-        public Task<Content<LaunchResponse>> GetAsync(FilterOption filterOption)
-        {
-            return GetAsync(filterOption, CancellationToken.None);
         }
 
         public async Task<Content<LaunchResponse>> GetAsync(CancellationToken cancellationToken)
@@ -135,16 +80,6 @@ namespace ReportPortal.Client.Resources
             }
 
             return await GetAsJsonAsync<Content<LaunchResponse>>(uri, cancellationToken).ConfigureAwait(false);
-        }
-
-        public Task<Content<LaunchResponse>> GetDebugAsync()
-        {
-            return GetDebugAsync(filterOption: null, CancellationToken.None);
-        }
-
-        public Task<Content<LaunchResponse>> GetDebugAsync(FilterOption filterOption)
-        {
-            return GetDebugAsync(filterOption, CancellationToken.None);
         }
 
         public async Task<Content<LaunchResponse>> GetDebugAsync(CancellationToken cancellationToken)
