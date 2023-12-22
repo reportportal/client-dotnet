ada1b4062223af3bfc3fbbc3fe5a1f7be45f855f
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2023-12-22 21:54:03 +0300

    Remove DataContract usage

--- src/ReportPortal.Client/Abstractions/Requests/AssignTestItemIssuesRequest.cs
+++ src/ReportPortal.Client/Abstractions/Requests/AssignTestItemIssuesRequest.cs
@@ -1,35 +1,29 @@
 ﻿using ReportPortal.Client.Abstractions.Responses;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace ReportPortal.Client.Abstractions.Requests
 {
     /// <summary>
     /// Defines a request for assigning issues to test items.
     /// </summary>
-    [DataContract]
     public class AssignTestItemIssuesRequest
     {
         /// <summary>
         /// List of test items and their issues.
         /// </summary>
-        [DataMember(Name = "issues")]
         public List<TestItemIssueUpdate> Issues { get; set; }
     }
 
-    [DataContract]
     public class TestItemIssueUpdate
     {
         /// <summary>
         /// A issue of test item.
         /// </summary>
-        [DataMember(Name = "issue")]
         public Issue Issue { get; set; }
 
         /// <summary>
         /// ID of test item to assign the issue.
         /// </summary>
-        [DataMember(Name = "testItemId")]
         public long TestItemId { get; set; }
     }
 }
--- src/ReportPortal.Client/Abstractions/Responses/Project/PreferenceResponse.cs
+++ src/ReportPortal.Client/Abstractions/Responses/Project/PreferenceResponse.cs
@@ -1,21 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.Runtime.Serialization;
 
 namespace ReportPortal.Client.Abstractions.Responses.Project
 {
-    [DataContract]
     public class PreferenceResponse
     {
         /// <summary>
         /// List of filters in a preference.
         /// </summary>
-        [DataMember(Name = "filters")]
         public IList<UserFilterResponse> Filters { get; set; }
 
-        [DataMember(Name = "projectId")]
         public long ProjectId { get; set; }
 
-        [DataMember(Name = "userId")]
         public long UserId { get; set; }
     }
 }
