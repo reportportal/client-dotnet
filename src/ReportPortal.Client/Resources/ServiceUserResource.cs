9755d2a12e95f2db412203dd956fe9da479ce5d2
Nikolay Borisenko <22616990+nvborisenko@users.noreply.github.com>
2024-10-01 14:29:24 +0300

    Update endpoint to return information about current logged-in user

--- src/ReportPortal.Client/Resources/ServiceUserResource.cs
+++ src/ReportPortal.Client/Resources/ServiceUserResource.cs
@@ -14,7 +14,7 @@ namespace ReportPortal.Client.Resources
 
         public async Task<UserResponse> GetAsync(CancellationToken cancellationToken)
         {
-            return await GetAsJsonAsync<UserResponse>("v1/user", cancellationToken).ConfigureAwait(false);
+            return await GetAsJsonAsync<UserResponse>("users", cancellationToken).ConfigureAwait(false);
         }
     }
 }
